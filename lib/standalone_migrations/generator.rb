7a6470afe8cae2fdc0e62c3c72cd723baa0eb70b
= <=>
2013-01-25 13:36:40 -0500

    changes

--- lib/standalone_migrations/generator.rb
+++ lib/standalone_migrations/generator.rb
@@ -9,15 +9,24 @@ module StandaloneMigrations
       generator_params = [name] + options.split(" ")
 
       puts "generator_params: #{generator_params}"
-      puts "Here are the files located in Rails.root: #{Rails.root}"
 
-      files = Dir.glob(File.join(Rails.root, "*"))
+      migration_path = Rails.root.join("db/migrate")
+      puts "Here are the OLD files located in the migration path: #{migration_path}"
+
+      files = Dir.glob(File.join(migration_path, "*"))
       files.each do |f|
         puts f
       end
       
       Rails::Generators.invoke "active_record:migration", generator_params,
         :destination_root => Rails.root
+
+      puts "Here are the NEW files located in the migration path: #{migration_path}"
+
+      files = Dir.glob(File.join(migration_path, "*"))
+      files.each do |f|
+        puts f
+      end
     end
   end
 end
